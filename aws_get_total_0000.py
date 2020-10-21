c1563c36fc1fbc0c2576e792fed8513f60c0145c
Bharathreddy1981 <70434245+Bharathreddy1981@users.noreply.github.com>
2020-10-21 19:37:20 +0530

    Update aws_get_total_0000.py

--- aws_get_total_0000.py
+++ aws_get_total_0000.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,jsonify
 import logging
 import boto3
 from botocore.exceptions import ClientError
@@ -22,7 +22,7 @@ def create_presigned_url():
     )
     my_bucket = s3_client.Bucket('flask121')
 
-    #list= []
+    list= []
     for file in my_bucket.objects.all():
         value = file.key
         # print(a)
@@ -48,11 +48,9 @@ def create_presigned_url():
             return None
 
 
-        #list.append({value: response})
+        list.append({value: response})
 
-    return  response
-
-
+    return jsonify(list)
 
 
 if __name__=="__main__":
